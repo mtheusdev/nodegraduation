c79d3ad2c1ec9245aff3f9c7621d64b55cd4b22c
Matheus Henrique <08990349931@edu.udesc.br>
2021-02-13 16:44:56 -0300

    sequelize, mysql, body-parser, formulários

--- perguntas/index.js
+++ perguntas/index.js
@@ -1,23 +1,36 @@
-const express = require("express");
+const express = require("express")
 const app = express();
+const bodyParser = require("body-parser")
+const connection = require("./database/database")
+const perguntaModel = require("./database/Pergunta")
+    //DATABASE
+connection.authenticate().then(() => {
+    console.log("Conexão feita com o banco de dados")
+}).catch((err) => {
+    console.log(err)
+})
 
 app.set('view engine', 'ejs') // Definindo o ejs como motor (renderizador) de html do express 
+    // html's devem ser salvos na pasta view (padrão express)
+app.use(express.static('public')); // para usar arquivos estaticos
 
-// html's devem ser salvos na pasta view (padrão express)
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json()) // ler dados via json
 
-app.get('/:nome/:lang', (req, res) => {
-    var nome = req.params.nome
-    var lang = req.params.lang
-    var msg = false
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "NeonDevSol",
-        inscritos: 3000,
-        msg: msg
-    });
+
+app.get('/', (req, res) => {
+    res.render("index");
 });
 
+app.get('/perguntar', (req, res) => {
+    res.render("perguntar");
+});
+
+app.post('/salvarpergunta', (req, res) => {
+    var titulo = req.body.titulo
+    var descricao = req.body.descricao
+    res.send(titulo + ' ' + descricao)
+});
 
 app.listen(3000, () => {
     console.log("Servidor rodando!");
