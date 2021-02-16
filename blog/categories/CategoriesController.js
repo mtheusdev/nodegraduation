8e10e1cc7eb8f71f6af1e9024a4d85575cce7182
Matheus Henrique <08990349931@edu.udesc.br>
2021-02-16 17:47:03 -0300

    blog

--- blog/categories/CategoriesController.js
+++ blog/categories/CategoriesController.js
@@ -13,10 +13,55 @@ router.post("/categories/save", (req, res) => {
         Category.create({
             title: title,
             slug: slugify(title)
-        }).then(() => res.redirect("/"))
+        }).then(() => res.redirect("/admin/categories"))
     } else {
         res.redirect("admin/categories/new")
     }
 })
+
+router.get("/admin/categories", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/categories/index", {
+            categories: categories
+        })
+    })
+})
+
+router.post("/categories/delete", (req, res) => {
+    var id = req.body.id
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories")
+            })
+        } else { // id nao numerico
+            res.redirect("/admin/categories")
+        }
+    } else { // id null
+        res.redirect("/admin/categories")
+    }
+})
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    var id = req.params.id;
+
+    if (isNaN(id)) {
+        res.redirect("/admin/categories");
+    }
+
+    Category.findByPk(id).then(category => {
+        if (category != undefined) {
+            res.render("admin/categories/edit", { category: category });
+        } else {
+            res.redirect("/admin/categories");
+        }
+    }).catch(erro => {
+        res.redirect("/admin/categories");
+    })
+});
 
 module.exports = router
