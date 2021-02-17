997e279f67a608bd62611461acae792c7e0fbd6a
Matheus Henrique <08990349931@edu.udesc.br>
2021-02-17 17:42:48 -0300

    seila

--- blog/articles/ArticlesController.js
+++ blog/articles/ArticlesController.js
@@ -1,8 +1,127 @@
 const express = require('express');
 const router = express.Router();
+const Article = require("../articles/Article")
+const slugify = require("slugify");
+const Category = require("../categories/Category");
 
-router.get("/articles", (req, res) => {
-    res.send("Rota de categorias")
+router.get("/admin/articles", (req, res) => {
+    Article.findAll({
+        include: [{ model: Category }]
+    }).then(articles => {
+        res.render("admin/articles/index", { articles: articles })
+    });
+});
+
+router.get("/admin/articles/new", (req, res) => {
+    Category.findAll().then(categories => {
+        res.render("admin/articles/new", {
+            categories: categories
+        });
+    })
+
+})
+
+router.post("/articles/delete", (req, res) => {
+    var id = req.body.id
+    if (id != undefined) {
+        if (!isNaN(id)) {
+            Article.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/articles")
+            })
+        } else { // id nao numerico
+            res.redirect("/admin/articles")
+        }
+    } else { // id null
+        res.redirect("/admin/articles")
+    }
+})
+
+router.post("/articles/save", (req, res) => {
+    var title = req.body.title
+    var body = req.body.body
+    var category = req.body.category
+
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/admin/articles")
+    })
+})
+
+router.post("/articles/update", (req, res) => {
+
+    var id = req.body.id
+    var title = req.body.title
+    var body = req.body.body
+    var category = req.body.category
+
+    Article.update({
+        title: title,
+        body: body,
+        categoryId: category,
+        slug: slugify(title)
+    }, {
+        where: {
+            id: id
+        }
+    }).then(() => {
+        res.redirect("/admin/articles")
+    }).catch(err => {
+        res.redirectres.redirect("?")
+    })
+})
+
+router.get("/admin/articles/edit/:id", (req, res) => {
+    var id = req.params.id
+    Article.findByPk(id).then(article => {
+        if (article != undefined) {
+            Category.findAll().then(categories => {
+                res.render("admin/articles/edit", {
+                    categories: categories,
+                    article: article
+                })
+            })
+        } else {
+            res.redirect("/")
+        }
+    }).catch(err => {
+        res.redirect("/")
+    })
+})
+
+router.get("/articles/page/:num", (req, res) => {
+    var page = req.params.num
+    var offset = 0
+    if (isNaN(page) || page == 1) {
+        offset = 0
+    } else {
+        offset = (parseInt(page) - 1) * 4
+    }
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset
+    }).then(articles => {
+        var next;
+        if (offset + 4 >= articles.count) {
+            next = false
+        } else {
+            next = true
+        }
+
+        var result = {
+            next: next,
+            article: articles
+        }
+
+        res.json(result)
+    })
 })
 
 module.exports = router
--- blog/index.js
+++ blog/index.js
@@ -4,8 +4,10 @@ const bodyParser = require("body-parser")
 const connection = require("./database/database")
 const categoriesController = require("./categories/CategoriesController");
 const articlesController = require("./articles/ArticlesController");
+
 const Article = require("./articles/Article")
 const Category = require("./categories/Category")
+
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
@@ -23,7 +25,61 @@ app.use("/", articlesController)
 
 
 app.get("/", (req, res) => {
-    res.render("index");
+    Article.findAll({
+        order: [
+            ["id", "desc"]
+        ]
+    }).then(articles => {
+        Category.findAll().then(categories => {
+            res.render("index", {
+                articles: articles,
+                categories: categories
+            })
+        })
+
+    })
+})
+
+app.get("/:slug", (req, res) => {
+    var slug = req.params.slug
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article => {
+        if (article != undefined) {
+            Category.findAll().then(categories => {
+                res.render("article", {
+                    article: article,
+                    categories: categories
+                })
+            })
+        } else {
+            res.redirect("/")
+        }
+    }).catch(erro => {
+        res.redirect("/")
+    })
+})
+
+app.get("/category/:slug", (req, res) => {
+    var slug = req.params.slug
+    Category.findOne({
+        where: {
+            slug: slug
+        },
+        include: [{ model: Article }]
+    }).then(category => {
+        if (category != undefined) {
+            Category.findAll().then(categories => {
+                res.render("index", { articles: category.articles, categories: categories })
+            })
+        } else {
+            res.redirect("/")
+        }
+    }).catch(err => {
+        res.redirect("/")
+    })
 })
 
 app.listen(8080, () => {
