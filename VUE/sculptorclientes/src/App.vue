894c137e0fc1abf7f73fc9ff60eba580dee175ac
Matheus Henrique <08990349931@edu.udesc.br>
2021-02-20 18:19:03 -0300

    vue js

---
+++ VUE/sculptorclientes/src/App.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="app">
+    <h3>Cadastro: </h3>
+    <input type="text" placeholder="nome" v-model="nomeField"><br>
+    <input type="email" placeholder="email" v-model="emailField">
+    <button @click="cadastrarUsuario">Cadastrar</button>
+  <div v-for="(cliente,index) in clientes" :key="index"> 
+     <Cliente :cliente="cliente"/>
+  </div>
+  </div>
+</template>
+
+<script>
+
+import Cliente from './components/Cliente.vue'
+
+export default {
+  name: 'App',
+  data() {
+    return{
+      nomeField:"",
+      emailField:"",
+
+      clientes: [
+        {
+          nome: "Matheus",
+          email: "matheus@gmail.com",
+          idade: "22",
+          numero:"14"
+        },
+        {
+          nome: "Matheus2",
+          email: "matheusgmail",
+          idade: "10",
+          numero:"13"
+        },
+        {
+          nome: "Matheus2sad",
+          email: "matheusgmailsadas",
+          idade: "102",
+          numero:"12"
+        },
+      ] 
+    }
+  },
+  components: {
+    Cliente
+  },
+  methods:{
+    cadastrarUsuario: function(){
+      if(this.nomeField == "" || this.nomeField == " "){
+        
+      }
+      this.clientes.push({
+          nome:this.nomeField,
+          email:this.emailField,
+          
+      }),
+      this.nomeField = "",
+      this.emailField = ""
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ VUE/sculptorclientes/src/components/Cliente.vue
@@ -0,0 +1,51 @@
+<template> 
+    <div :class="{'cliente': !isPremium, 'cliente-premium': isPremium}">
+        <h1>{{cliente.nome}}</h1>
+        <hr>
+        <p>{{cliente.email}}</p>
+        <hr>
+        <p>{{cliente.numero}}</p>
+        <hr>
+        <p v-if="cliente.idade != undefined">{{cliente.idade}}</p>
+        <p v-else>O usuário nao tem idade</p>
+        <button @click="mudarCor($event)">Mudar cor!</button>
+    </div>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            isPremium: false
+        };
+    },
+    props:{
+        cliente: Object
+
+    },
+    methods:{
+        mudarCor: function($event){
+            this.isPremium = !this.isPremium
+            console.log($event)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .cliente{
+        background-color: rgb(168, 143, 111);
+        max-width: 600px;
+        height: auto;
+        padding: 1%;
+        margin-top:2%
+    }
+    .cliente-premium{
+        background-color: rgb(155, 155, 155);
+        max-width: 600px;
+        height: auto;
+        padding: 1%;
+        margin-top:2%
+    }
+</style>
