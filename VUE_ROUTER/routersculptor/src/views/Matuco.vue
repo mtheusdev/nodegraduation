b60103319073ac650f331c0e5676dab0588c5ece
Matheus Henrique <08990349931@edu.udesc.br>
2021-02-22 14:37:08 -0300

    Vue Router

---
+++ VUE_ROUTER/routersculptor/src/views/Matuco.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <input type="text" name="" id="" placeholder="lalala">
+    <h3>{{parame}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    created(){
+        console.log(this.$route.params.id);
+        this.parame = this.$route.params.id
+    },
+    data()
+    {
+        return{
+            parame: "Nada"
+        }        
+    }
+}
+</script>
